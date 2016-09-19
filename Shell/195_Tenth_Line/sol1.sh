924717a82b1f8320c2fe197bd75f9fe5ea5a392d
RYWU <b00902048@ntu.edu.tw>
2016-09-20 01:47:00 +0800

    Update Tenth_Line: sol1.sh, using head, tail.

--- Shell/195_Tenth_Line/sol1.sh
+++ Shell/195_Tenth_Line/sol1.sh
@@ -1,2 +1,9 @@
+# Time: 
+# Space: 
 # get the first 10 lines of a file, and then print the last line of the 10 lines, which was exactly the 10th line.
-head -n 10 file.txt | tail -n 1
+if [ `wc file.txt | awk {'print $1'}` -lt "10" ]; then
+    echo "file.txt have less than 10 lines."
+else
+    head -n 10 file.txt | tail -n 1
+fi
+
