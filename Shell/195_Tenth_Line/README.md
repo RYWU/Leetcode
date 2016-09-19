48d6a28d868f4ceb11c11887b67ef592b57a7fe6
RYWU <b00902048@ntu.edu.tw>
2016-09-20 01:55:17 +0800

    Update Tenth_Line: print '' when file.txt have more than 10 lines.

--- Shell/195_Tenth_Line/README.md
+++ Shell/195_Tenth_Line/README.md
@@ -1,2 +1,3 @@
 # Problem domain
 1. What if the file.txt contains less than 10 lines?
+* You should print "".
--- Shell/195_Tenth_Line/sol1.sh
+++ Shell/195_Tenth_Line/sol1.sh
@@ -2,7 +2,7 @@
 # Space: 
 # get the first 10 lines of a file, and then print the last line of the 10 lines, which was exactly the 10th line.
 if [ `wc file.txt | awk {'print $1'}` -lt "10" ]; then
-    echo "file.txt have less than 10 lines."
+    echo ""
 else
     head -n 10 file.txt | tail -n 1
 fi
